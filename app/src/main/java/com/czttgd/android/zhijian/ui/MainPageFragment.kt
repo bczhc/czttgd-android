3a7bc3851db1595f03294dae7b732dcbdb51c6b4
Zhai Can <bczhc0@126.com>
2024-06-29 03:18:22 +0800

    ui: form filling activity

--- app/src/main/java/com/czttgd/android/zhijian/ui/MainPageFragment.kt
+++ app/src/main/java/com/czttgd/android/zhijian/ui/MainPageFragment.kt
@@ -1,5 +1,6 @@
 package com.czttgd.android.zhijian.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,11 @@ import com.czttgd.android.zhijian.databinding.FragmentMainPageBinding
 class MainPageFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val bindings = FragmentMainPageBinding.inflate(inflater, container, false)
+
+        bindings.scanButton.setOnClickListener {
+            startActivity(Intent(requireContext(), FormFillingActivity::class.java))
+        }
+
         return bindings.root
     }
 }
