3f2d2b9e8c7e06c0323ebe2906eff2f7839dbde4
Zhai Can <bczhc0@126.com>
2024-07-20 02:19:58 +0800

    FormFillingActivity: click to input text

--- app/src/main/java/com/czttgd/android/zhijian/ui/FormFillingActivity.kt
+++ app/src/main/java/com/czttgd/android/zhijian/ui/FormFillingActivity.kt
@@ -2,18 +2,43 @@ package com.czttgd.android.zhijian.ui
 
 import android.os.Bundle
 import com.czttgd.android.zhijian.BaseActivity
+import com.czttgd.android.zhijian.R
 import com.czttgd.android.zhijian.databinding.ActivityFormFillingBinding
+import com.czttgd.android.zhijian.databinding.DialogInputTextBinding
+import com.czttgd.android.zhijian.databinding.FormFillingFieldLayoutBinding
+import com.czttgd.android.zhijian.utils.defaultNegativeButton
+import com.czttgd.android.zhijian.utils.setPositiveAction
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class FormFillingActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bindings = ActivityFormFillingBinding.inflate(layoutInflater)
 
+        setContentView(bindings.root)
+
         bindings.toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-        setContentView(bindings.root)
+        val setUpClickEvent = { fieldBindings: FormFillingFieldLayoutBinding ->
+            fieldBindings.tv.setOnClickListener {
+                val dialogBindings = DialogInputTextBinding.inflate(layoutInflater)
+                MaterialAlertDialogBuilder(this)
+                    .defaultNegativeButton()
+                    .setPositiveAction { _, _ ->
+                        fieldBindings.tv.text = dialogBindings.et.text.toString()
+                    }
+                    .setTitle(getString(R.string.please_enter_text_dialog_title, fieldBindings.labelTv.text))
+                    .setView(dialogBindings.root)
+                    .create().apply {
+                        setCanceledOnTouchOutside(false)
+                    }
+                    .show()
+            }
+        }
+
+        listOf(bindings.fieldProductSpecs, bindings.fieldWireNumber, bindings.fieldComments).forEach(setUpClickEvent)
     }
 
     companion object {
---
+++ app/src/main/java/com/czttgd/android/zhijian/ui/SelectionActivity.kt
@@ -0,0 +1,10 @@
+package com.czttgd.android.zhijian.ui
+
+import android.os.Bundle
+import com.czttgd.android.zhijian.BaseActivity
+
+class SelectionActivity: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
