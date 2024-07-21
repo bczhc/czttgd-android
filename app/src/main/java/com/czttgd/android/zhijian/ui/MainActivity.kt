966a42f054b75e4fbcc4e3c411481b8f1346a77a
Zhai Can <bczhc0@126.com>
2024-07-21 23:05:48 +0800

    ui: InspectionRecordsFragment

--- app/src/main/java/com/czttgd/android/zhijian/ui/MainActivity.kt
+++ app/src/main/java/com/czttgd/android/zhijian/ui/MainActivity.kt
@@ -1,28 +1,14 @@
 package com.czttgd.android.zhijian.ui
 
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AlertDialog
-import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import androidx.lifecycle.lifecycleScope
 import com.czttgd.android.zhijian.BaseActivity
 import com.czttgd.android.zhijian.R
-import com.czttgd.android.zhijian.data.Ping
-import com.czttgd.android.zhijian.data.Settings
 import com.czttgd.android.zhijian.databinding.ActivityMainBinding
-import com.czttgd.android.zhijian.databinding.SettingsDialogBinding
-import com.czttgd.android.zhijian.ui.fragments.BreakpointRecordsFragment
+import com.czttgd.android.zhijian.ui.fragments.InspectionRecordsFragment
 import com.czttgd.android.zhijian.ui.fragments.WorkspaceFragment
-import com.czttgd.android.zhijian.utils.defaultNegativeButton
-import com.czttgd.android.zhijian.utils.setPositiveAction
-import com.czttgd.android.zhijian.utils.toast
 import com.czttgd.android.zhijian.utils.unreachable
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +25,7 @@ class MainActivity : BaseActivity() {
         bindings.bottomNavBar.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.workspace -> updateFragment(WorkspaceFragment())
-                R.id.records -> updateFragment(BreakpointRecordsFragment())
+                R.id.records -> updateFragment(InspectionRecordsFragment())
                 else -> unreachable()
             }
             true
