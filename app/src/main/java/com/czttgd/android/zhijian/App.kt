620be3e2bf82fb2d8491ba8e17e9be7b8a4451ac
Zhai Can <bczhc0@126.com>
2024-07-25 15:23:54 +0800

    ktor: change request timeout to 5s
    The server may suck. Make a larger tolerance.

--- app/src/main/java/com/czttgd/android/zhijian/App.kt
+++ app/src/main/java/com/czttgd/android/zhijian/App.kt
@@ -38,6 +38,6 @@ val GSON by lazy { App.GSON }
 val appHttpClient: HttpClient
     get() = HttpClient {
         install(HttpTimeout) {
-            requestTimeoutMillis = 2000
+            requestTimeoutMillis = 5000
         }
     }
