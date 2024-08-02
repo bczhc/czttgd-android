a144628c57d951acf9d53c21d685d617031f793f
Zhai Can <bczhc0@126.com>
2024-08-02 14:06:30 +0800

    ktor: fix empty form-data when in release build

--- app/src/main/java/com/czttgd/android/zhijian/utils/Ktor.kt
+++ app/src/main/java/com/czttgd/android/zhijian/utils/Ktor.kt
@@ -5,13 +5,18 @@ import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import kotlin.reflect.KVisibility
 import kotlin.reflect.full.memberProperties
 
 object FormDataUtils {
     inline fun <reified T : Any> fromObject(obj: T): FormDataContent {
         return FormDataContent(Parameters.build {
-            T::class.memberProperties.filter { it.visibility == KVisibility.PUBLIC }.forEach {
+            T::class.memberProperties.filter {
+                // all property visibilities will become PRIVATE when the app is built in release mode
+                // kinda weird, and I don't know why, plus I haven't found any relevant issue on the Internet
+                // Just comment this out at this moment.
+                // it.visibility == KVisibility.PUBLIC
+                true
+            }.forEach {
                 val name = it.name
                 val value = it.getter.call(obj)
                 value?.let { v ->
